30244d329e872132e6e7ae966bf3211c2dc766d5
Ryan Attard <ryanattard@gmail.com>
2025-10-06 10:46:34 +0200

    Complete Architecture

---
+++ EP_SWD62B_20252026/Domain/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public  class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public double WholesalePrice { get; set; }
+        public int Section { get; set; }
+        public string OrderedFrom { get; set; }
+    }
+}
---
+++ EP_SWD62B_20252026/Presentation/Models/BookUserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Presentation.Models
+{
+    public class BookUserViewModel
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+    }
+}
